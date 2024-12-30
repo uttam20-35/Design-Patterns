11548faca65f8855b8ecaefeba92cc8558811724
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2024-12-30 20:41:10 +0530

    Update Shop.java

--- src/com/creational/builder/Shop.java
+++ src/com/creational/builder/Shop.java
@@ -3,13 +3,15 @@ package com.creational.builder;
 public class Shop {
 
     public static void main(String[] args) {
-       //Without Builder Pattern:
-        // if any value not passed in parameter then gets a compilation error.
+        //Without Builder Pattern:
+            // if any value not passed in parameter then gets a compilation error.
+        System.out.println("Without Builder Pattern:");
         Phone phone= new Phone("Android",8 ,"MediaTek", 5.5,3000);
         System.out.println(phone);
 
         //With Builder Pattern:
             // We can pass required data members into the phone, not required to pass all the members,
+        System.out.println("Using Builder Pattern:");
         Phone phoneOne= new PhoneBuilder().setOs("IOS").setBattery(5000).setRam(16).getPhone();
         System.out.println(phoneOne);
     }
