0f82f4100f5210db5b42ce6207dd4f69cc0a014f
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2024-12-30 20:38:57 +0530

    Update Shop.java
    Call phone builder to acquire the concept of creational-builder-pattern.

--- src/com/creational/builder/Shop.java
+++ src/com/creational/builder/Shop.java
@@ -7,5 +7,10 @@ public class Shop {
         // if any value not passed in parameter then gets a compilation error.
         Phone phone= new Phone("Android",8 ,"MediaTek", 5.5,3000);
         System.out.println(phone);
+
+        //With Builder Pattern:
+            // We can pass required data members into the phone, not required to pass all the members,
+        Phone phoneOne= new PhoneBuilder().setOs("IOS").setBattery(5000).setRam(16).getPhone();
+        System.out.println(phoneOne);
     }
 }
